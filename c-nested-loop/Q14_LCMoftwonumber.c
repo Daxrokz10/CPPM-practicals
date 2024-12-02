69f9d665390405de4ade8e17f6b4bf277122d042
Daxrokz10 <dakshgagnani@gmail.com>
2024-12-02 12:12:31 +0530

    pyramid is one sided bruh

---
+++ c-nested-loop/Q14_LCMoftwonumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,temp;
+
+    printf("Enter number 1 for LCM: ");
+    scanf("%d",&a);
+    printf("Enter number 2 for LCM: ");
+    scanf("%d",&b);
+
+    for(int i=1;i<=c;i++){
+        
+    }
+}
